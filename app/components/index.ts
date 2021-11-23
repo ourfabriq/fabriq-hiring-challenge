19e4b35301b4686f202dce97634b372f7a106dc3
Lucas Whitman <lucasmichaelwhitman@gmail.com>
2021-11-23 14:36:26 -0700

    that too

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,7 +1,6 @@
 export * from "./bullet-item/bullet-item"
 export * from "./button/button"
 export * from "./checkbox/checkbox"
-export * from "./form-row/form-row"
 export * from "./header/header"
 export * from "./icon/icon"
 export * from "./screen/screen"
